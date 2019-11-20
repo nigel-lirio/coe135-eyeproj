3a865b5c5f7104de29b3500cc8f6dbd1b2382b06
nigel <nigel_lirio@yahoo.com>
2019-11-20 23:25:55 +0800

    Face Detection

---
+++ Makefile
@@ -0,0 +1,9 @@
+CPP_FLAGS=-std=c++11
+OPENCV_LIBS: -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_objdetect -lopencv_imgcodecs -lopencv_videoio
+LD_FLAGS=$(OPENCV_LIBS)
+
+default: EyeDetector
+EyeDetector: eye_detector.cpp
+  g++ $(CPP_FLAGS) $^ -o eyep $(LD_FLAGS)
+clean:
+  rm -f EyeDetector
---
+++ eye_detector.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp> 
+
+void detectEyes(cv::Mat &frame, cv::CascadeClassifier &faceCascade, cv::CascadeClassifier &eyeCascade)
+{
+  cv::Mat grayscale;
+  cv::cvtColor(frame, grayscale, CV_BGR2GRAY); // convert image to grayscale
+  cv::equalizeHist(grayscale, grayscale); // enhance image contrast 
+  std::vector<cv::Rect> faces;
+  faceCascade.detectMultiScale(grayscale, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(150, 150));
+  if (faces.size() == 0) return; // none face was detected
+  cv::Mat face = frame(faces[0]); // crop the face
+  std::vector<cv::Rect> eyes;
+  eyeCascade.detectMultiScale(face, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(150, 150)); // same thing as above  
+  rectangle(frame, faces[0].tl(), faces[0].br(), cv::Scalar(255, 0, 0), 2);
+  if (eyes.size() != 2) return; // both eyes were not detected
+  for (cv::Rect &eye : eyes)
+  {
+      rectangle(frame, faces[0].tl() + eye.tl(), faces[0].tl() + eye.br(), cv::Scalar(0, 255, 0), 2);
+  }
+}
+
+int main()
+{
+  cv::CascadeClassifier faceCascade;
+  cv::CascadeClassifier eyeCascade;
+  if (!faceCascade.load("./haarcascade_frontalface_alt.xml"))
+  {
+      std::cerr << "Could not load face detector." << std::endl;
+      return -1;
+  }
+  if (!eyeCascade.load("./haarcascade_eye.xml"))
+  {
+      std::cerr << "Could not load eye detector." << std::endl;
+      return -1;
+  }
+  cv::VideoCapture cap(0); // the fist webcam connected to your PC
+  if (!cap.isOpened())
+  {
+      std::cerr << "Webcam not detected." << std::endl;
+      return -1;
+  }
+  cv::Mat frame;
+  while (1)
+  {
+      cap >> frame; // outputs the webcam image to a Mat
+      detectEyes(frame, faceCascade, eyeCascade);
+      cv::imshow("Webcam", frame); // displays the Mat
+      cvWaitKey(30);
+  }
+  return 0;
+}
